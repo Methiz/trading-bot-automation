c1282855c6059d689a357e1661c054f24ede67fd
Omar Faruq Pijon <pizonpocket@gmail.com>
2026-07-05 03:09:42 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# ============== CONFIG ==============
+TELEGRAM_TOKEN = "Yourbottoken"
+TELEGRAM_CHAT_ID = "youttgid"
+
+PAIRS = ["EURUSD=X", "USDJPY=X", "GBPUSD=X", "USDCHF=X", "AUDUSD=X"]
+
+AMOUNT = 10
+DURATION = 120  # seconds
+
+# Risk & Backtest
+INITIAL_CAPITAL = 1000.0
+MAX_DAILY_RISK_FRACTION = 0.02
